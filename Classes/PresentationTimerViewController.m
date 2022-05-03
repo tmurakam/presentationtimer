7acbf957c69c8e3ce0bdf172230985407ad5f02c
Takuya Murakami <tmurakam@tmurakam.org>
2022-05-03 16:45:19 +0900

    chore: Fix invertCountDown signature

--- Classes/PresentationTimerViewController.m
+++ Classes/PresentationTimerViewController.m
@@ -1,7 +1,7 @@
 /*
   Presentation Timer for iOS
 
-  Copyright (c) 2008-2018, Takuya Murakami, All rights reserved.
+  Copyright (c) 2008-2022, Takuya Murakami, All rights reserved.
 
   Redistribution and use in source and binary forms, with or without
   modification, are permitted provided that the following conditions are
@@ -61,7 +61,7 @@
 - (IBAction)bellButtonTapped:(id)sender;
 - (IBAction)manualBell:(id)sender;
 - (IBAction)showHelp:(id)sender;
-- (IBAction)invertCountDown:(id)sender;
+- (void)invertCountDown;
 
 - (void)updateButtonTitle;
 - (void)updateTimeLabel;
@@ -220,14 +220,14 @@
 {
     UITouch *touch = [event.allTouches anyObject];
     if (touch.view.tag == timeLabel.tag) {
-        [self invertCountDown:timeLabel];
+        [self invertCountDown];
     }
 }
 
 /**
    Toggle count down mode
 */
-- (IBAction)invertCountDown:(id)sender
+- (void)invertCountDown
 {
     mIsCountDown = !mIsCountDown;
     [self updateTimeLabel];
